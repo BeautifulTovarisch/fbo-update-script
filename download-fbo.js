38f0935241e375d27d3c7cbb376cb188124fc38c
Anthony Re <re@towson.edu>
2018-07-12 14:44:03 -0400

    Initial Commit

---
+++ download-fbo.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const fs = require( 'fs' );
+const fcsv = require( 'fast-csv' );
+const Client = require( 'ftp' );
+const csvWriter = require( 'csv-write-stream' );
+
+const c = new Client();
+
+const fboFile = "/FBORecovery/FBORecovery20180707.csv";
+
+const options = {
+    host: "ftp.fbo.gov"
+};
+
+const maxDate = ( a, b ) => new Date( a.date ) > new Date( b.date ) ? a : b;
+
+c.on( 'ready', () => {
+    c.cwd( "FBORecovery", ( err, currentDir ) => {
+	c.list( ( err, list ) => {
+	    if( err )
+		throw err;
+
+	    const latestRecord = list.reduce( maxDate );
+
+	    c.get( latestRecord.name, ( err, stream ) => {
+		if( err )
+		    throw err;
+
+		const csvStream = fcsv.parse({
+		    quote: '"',
+		    headers: true,
+		    delimiter: ","
+		});
+
+		const writer = csvWriter( { separator: '\0z' } );
+		writer.pipe( fs.createWriteStream( latestRecord.name ) );
+
+		stream.once( 'close', () => c.end() );
+		stream
+		    .pipe( csvStream )
+		    .pipe( writer )
+		    .on( 'end', () => console.log( "Wrote file", latestRecord.name ) )
+		    .on( 'error', err => console.log( err ) );
+
+	    });
+	});
+    });
+});
+
+c.connect( options );
